6fe764075d5f291d80e25295d5d700b5cbd454ca
anconine <49389122+anconine@users.noreply.github.com>
2024-05-25 16:11:23 +0700

    Update DlgRptRincselisihpengadaanbmd.cs
    Penambahan link excel

--- Usadi/Usadi.Valid49.Aset.Rpt/BO/DlgRptRincselisihpengadaanbmd.cs
+++ Usadi/Usadi.Valid49.Aset.Rpt/BO/DlgRptRincselisihpengadaanbmd.cs
@@ -112,6 +112,22 @@ namespace Usadi.Valid49.BO
         return new ImageCommand[] { cmd1 };
       }
     }
+    public ImageCommand[] Cmds1
+    {
+      get
+      {
+        ImageCommand cmd2 = new ImageCommand()
+        {
+          CommandName = "LinkExcel",
+          Icon = Icon.PageExcel,
+          ToolTip =
+          {
+            Text = "Klik tombol ini untuk mencetak data"
+          }
+        };
+        return new ImageCommand[] { cmd2 };
+      }
+    }
 
 
     public override HashTableofParameterRow GetEntries()
@@ -179,6 +195,20 @@ namespace Usadi.Valid49.BO
         return ConstantDict.Translate(XMLName) + " " + Kdunit + "|" + url;
       }
     }
+    public string LinkExcel
+    {
+      get
+      {
+        string app = GlobalAsp.GetRequestApp();
+        string id = GlobalAsp.GetRequestId();
+        string idprev = GlobalAsp.GetRequestId();
+        string kode = GlobalAsp.GetRequestKode();
+        string idx = GlobalAsp.GetRequestIndex();
+        string strenable = "&enable=" + ((Status == 0) ? 1 : 0);
+        string url = string.Format("Page/PageRpt.aspx?passdc=1&app={0}&i=1&id={1}&idprev={2}&kode={3}&idx={4}&val=0" + strenable, app, id, idprev, kode, idx);
+        return ConstantDict.Translate(XMLName) + " " + Kdunit + "|" + url;
+      }
+    }
 
     public string GetURLReport(int n)
     {
@@ -204,7 +234,17 @@ namespace Usadi.Valid49.BO
       Params["@subunit"] = Subunit;
 
 
-      string pdfurl = ReportUtils.ExportPDF("Usadi.Valid49.Aset.Rpt.Rpt.REKAPRINCSELISIH_ASETTETAPINTRA.rpt", CS, Params);
+      //string pdfurl = ReportUtils.ExportPDF("Usadi.Valid49.Aset.Rpt.Rpt.REKAPRINCSELISIH_ASETTETAPINTRA.rpt", CS, Params);
+      string pdfurl;
+      if (n == 0)
+      {
+        pdfurl = ReportUtils.ExportPDF("Usadi.Valid49.Aset.Rpt.Rpt.REKAPRINCSELISIH_ASETTETAPINTRA.rpt", CS, Params);
+      }
+      else
+      {
+        CS[ReportUtils.PDFNAME] = "REKAPRINCSELISIH_ASETTETAPINTRA_" + (string)GlobalAsp.GetSessionUser().GetValue("Userid") + "_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xls";
+        pdfurl = ReportUtils.ExportExcel("Usadi.Valid49.Aset.Rpt.Rpt.REKAPRINCSELISIH_ASETTETAPINTRA.rpt", CS, Params);
+      }
       return pdfurl;
     }
 
@@ -219,8 +259,9 @@ namespace Usadi.Valid49.BO
 
     public List<RptPars> GetReports()
     {
-      RptPars rptPar = new RptPars() { Title = "Rekapitulasi Rincian Penjelasan Selisih Laporan Pengadaan BMD", RptClass = LinkPdf };
-      return new List<RptPars>(new RptPars[] { rptPar });
+      RptPars rptPar = new RptPars() { Title = "Pdf", RptClass = LinkPdf };
+      RptPars rptPar1 = new RptPars() { Title = "Excel", RptClass = LinkExcel };
+      return new List<RptPars>(new RptPars[] { rptPar, rptPar1 });
     }
 
     public void Print(int n)
